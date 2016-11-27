42160a2c6884c4979a81f8cea0ae91d55628e6c9
Alejandro Gallo <aamsgallo@gmail.com>
2016-11-27 17:28:02 +0100

    add dist to repo and add update function

--- dist/testaco.sh
+++ dist/testaco.sh
@@ -3,7 +3,10 @@
 include() { source $@; }
 
 # main logging functions
-include logging.sh
+function header()   { echo -e "\n\033[1m$@\033[0m"; }
+function success()  { echo -e " \033[1;32m==>\033[0m  $@"; }
+function error()    { echo -e " \033[1;31mX\033[0m  $@"; }
+function arrow()    { echo -e " \033[1;34m==>\033[0m  $@"; }
 
 # READONLY PARAMETERS
 declare -r TEST_OUT_FILE=test.out
